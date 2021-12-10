45bb91090ff74eac34429238689add70fe08125a
KaloyJS <carlojs@protonmail.com>
2021-12-09 21:20:55 -0500

    Added comments to error handler

--- app/Classes/ErrorHandler.php
+++ app/Classes/ErrorHandler.php
@@ -6,16 +6,18 @@ class ErrorHandler
 {
     public function handleErrors($error_number, $error_message, $error_file, $error_line)
     {
+        // create error message from arguments
         $error = "[{$error_number}] and error occurred in 
                     file {$error_file} on line {$error_line}: {$error_message}";
 
         $environment = $_ENV['APP_ENV'];
-
+        // if environment is local use Custom Error handler whoops
         if ($environment === 'localhost') {
             $whoops = new \Whoops\Run;
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
             $whoops->register();
         } else {
+            // if production send email to administrator and display error message
             $data = [
                 "to" => $_ENV['ADMIN_EMAIL'],
                 "subject" => "System Error",
