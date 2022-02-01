6fff63d6432bd54e4f2d87d9a4b8b0cf8695eb04
KaloyJS <carlojs@protonmail.com>
2022-01-31 22:06:57 -0500

    saving progress

---
+++ app/Controllers/Admin/ProductController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Classes\Request;
+use App\Classes\Session;
+use App\Classes\Utility;
+use App\Models\Category;
+use App\Classes\Redirect;
+use App\Classes\CSRFToken;
+use App\Models\SubCategory;
+use App\Classes\ValidateRequest;
+use App\Controllers\BaseController;
+
+
+class ProductController extends BaseController
+{
+    public $table_name = 'categories';
+    public $categories;
+    public $links;
+    public $subcategories;
+    public $subcategories_links;
+
+    public function __construct()
+    {
+        $this->categories = Category::all();        
+
+        // list($this->categories, $this->links) = paginate(6, $total, $this->table_name, $object);
+        // list($this->subcategories, $this->subcategories_links) = paginate(6, $subcategoriesTotal, 'sub_categories', $subcategories_object);
+    }
+
+    public function showCreateProductForm()
+    {
+        $categories = $this->categories;
+        return view('admin/products/create', compact('categories'));
+    }
+
+    public function store()
+    {
+        // check if post request exists
+        if (Request::has('post')) {
+            // gather all of $_POST values
+            $request = Request::get('post');
+
+            // check if request token is valid, process the form data            
+            if (CSRFToken::verifyCSRFToken($request->token)) {
+                $rules = [
+                    'name' => ['required' => true, 'minLength' => 3, 'string' => true, 'unique' => 'categories']
+                ];
+
+                $validate = new ValidateRequest();
+                $validate->abide($_POST, $rules);
+
+                // check if any validation errors occurred
+                if ($validate->hasError()) {
+                    $errors = $validate->getErrorMessages();
+                    return view('admin/products/categories', [
+                        'categories' => $this->categories,
+                        'links' => $this->links,
+                        'errors' => $errors,
+                        'subcategories' => $this->subcategories,
+                        'subcategories_links' => $this->subcategories_links
+                    ]);
+                }
+
+                Category::create([
+                    'name' => $request->name,
+                    'slug' => slug($request->name)
+                ]);
+
+                $total = count(Category::all());
+                list($this->categories, $this->links) = paginate(6, $total, $this->table_name, new Category());
+
+                $subcategories_object = new SubCategory;
+                $subcategoriesTotal = count(SubCategory::all());
+                list($this->subcategories, $this->subcategories_links) = paginate(6, $subcategoriesTotal, 'sub_categories', $subcategories_object);
+
+                return view('admin/products/categories', [
+                    'categories' => $this->categories,
+                    'links' => $this->links,
+                    'success' => 'Category Created',
+                    'subcategories' => $this->subcategories,
+                    'subcategories_links' => $this->subcategories_links
+                ]);
+            }
+            throw new \Exception('Token mismatch');
+        }
+
+        return null;
+    }
+
+    public function edit($id)
+    {
+        // check if post request exists
+        if (Request::has('post')) {
+            // gather all of $_POST values
+            $request = Request::get('post');
+
+            // check if request token is valid, process the form data            
+            if (CSRFToken::verifyCSRFToken($request->token, false)) {
+                $rules = [
+                    'name' => ['required' => true, 'minLength' => 3, 'string' => true, 'unique' => 'categories']
+                ];
+
+                $validate = new ValidateRequest();
+                $validate->abide($_POST, $rules);
+
+                // check if any validation errors occurred
+                if ($validate->hasError()) {
+                    $errors = $validate->getErrorMessages();
+                    header('HTTP/1.1 422 Unrpocessable Entity', true, 422);
+                    echo json_encode($errors);
+                    exit();
+                }
+
+                Category::where('id', $id)->update(['name' => $request->name]);
+                echo json_encode(['success' => 'Record Update Successfully']);
+                exit();
+            }
+            throw new \Exception('Token mismatch');
+        }
+
+        return null;
+    }
+
+    public function delete($id)
+    {
+        // check if post request exists
+        if (Request::has('post')) {
+            // gather all of $_POST values
+            $request = Request::get('post');
+
+            // check if request token is valid, process the form data            
+            if (CSRFToken::verifyCSRFToken($request->token)) {
+                Category::destroy($id);
+                // check if category has subcategories, 
+                // if so delete also subcategories
+                $subcategories = SubCategory::where('category_id', $id)->get();
+                if (count($subcategories)) {
+                    foreach ($subcategories as $key => $subcategory) {
+                        $subcategory->delete();
+                    }
+                }
+                // add success message to session
+                Session::add('success', 'Category Deleted');
+                Redirect::to('/admin/product/categories');
+            }
+            throw new \Exception('Token mismatch');
+        }
+
+        return null;
+    }
+}
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -13,8 +13,38 @@
             <div class="small-12 medium-11">
                 <div class="row expanded">
                     <div class="small-12 medium-6 column">
-                        <label for="">Product Name</label>
-                        <input type="text" name="name" placeholder="Product name" value={{ \App\Classes\Request::old('post', 'name')}}"">
+                        <label for="">Product Name:</label>
+                        <input type="text" name="name" placeholder="Product name" value="{{ \App\Classes\Request::old('post', 'name')}}">
+                    </div>
+
+                    <div class="small-12 medium-6 column">
+                        <label for="">Product Price:</label>
+                        <input type="text" name="price" placeholder="Product price" value="{{ \App\Classes\Request::old('post', 'price')}}">
+                    </div>
+
+                    <div class="small-12 medium-6 column">
+                        <label for="">Product Category:</label>
+                        <select name="category" id="product-category">
+                            <option value="{{ \App\Classes\Request::old('post', 'category') ?: ""}}">
+                                {{ \App\Classes\Request::old('post', 'category') ?: "Select Category"}}
+                            </option>
+                            @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        
+                    </div>
+
+                    <div class="small-12 medium-6 column">
+                        <label for="">Product Subcategory:</label>
+                        <select name="subcategory" id="product-subcategory">
+                            <option value="{{ \App\Classes\Request::old('post', 'subcategory') ?: ""}}">
+                                {{ \App\Classes\Request::old('post', 'subcategory') ?: "Select Subcategory"}}
+                            </option>
+                            @foreach($subcategories as $subcategory)
+                                <option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
                 </div>
             </div>
